186ca59098971198cf3293966ce88e73aac987ec
mateu <mateusz.wygoda>
2022-01-27 23:13:13 +0100

    Add full logic to project

--- src/MyCar.java
+++ src/MyCar.java
@@ -1,2 +1,12 @@
 public class MyCar {
+    public static void main(String [] args){
+        String mark = "Kia";
+        String model = "Rio";
+        int productionYear = 1990;
+        double price = 1403.2;
+
+        System.out.println("Mark: "+mark + " model: "+model+" year: "+productionYear+" price: "+price
+
+        );
+    }
 }
